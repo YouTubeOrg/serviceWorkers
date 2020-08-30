4582820b275d9ed94d7ffeb23d597a2c2e48453f
ezekieltech <ezekielobhafuoso@gmail.com>
2020-08-30 10:41:53 +0100

    Feature: Tested the listening of install and activated events as per the service worker lifecycle

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ if (navigator.serviceWorker) { // aliter if navigator.serviceWorker
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('../simple_service_worker/sw_cached_pages.js')
-      .then(reg => console.log('Service Worker: Registered'))
+      .then(reg => console.log('Service Worker: Registered')) 
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
   // console.log('worked')
--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -7,9 +7,9 @@
 //   '/js/main.js'
 // ];
 
-// // Call Install Event
-// self.addEventListener('install', e => {
-//   console.log('Service Worker: Installed');
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
 
 //   e.waitUntil(
 //     caches
@@ -20,11 +20,11 @@
 //       })
 //       .then(() => self.skipWaiting())
 //   );
-// });
+});
 
-// // Call Activate Event
-// self.addEventListener('activate', e => {
-//   console.log('Service Worker: Activated');
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
 //   // Remove unwanted caches
 //   e.waitUntil(
 //     caches.keys().then(cacheNames => {
@@ -38,10 +38,10 @@
 //       );
 //     })
 //   );
-// });
+});
 
 // // Call Fetch Event
 // self.addEventListener('fetch', e => {
 //   console.log('Service Worker: Fetching');
 //   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-// });
+// })
