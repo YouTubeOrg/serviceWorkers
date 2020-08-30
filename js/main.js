6d017c1bce9b44b8c1e9f6b02c1a537935c162f5
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2020-08-30 09:54:48 +0100

    Just started

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Make sure sw are supported
+if ('serviceWorker' in navigator) { // aliter if navigator.serviceWorker
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('../sw_cached_pages.js')
+      .then(reg => console.log('Service Worker: Registered (Pages)'))
+      .catch(err => console.log(`Service Worker: Error: ${err}`));
+  });
+}
