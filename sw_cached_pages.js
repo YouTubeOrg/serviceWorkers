a841a032bd9b371dba290ceb7bd26c79e485e0bd
ezekieltech <ezekielobhafuoso@gmail.com>
2020-08-30 11:09:11 +0100

    Feature: Cached set of data to a name when installing service worker

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,25 +1,25 @@
-// const cacheName = 'v1';
+const cacheName = 'v1';
 
-// const cacheAssets = [
-//   'index.html',
-//   'about.html',
-//   '/css/style.css',
-//   '/js/main.js'
-// ];
+const cacheAssets = [
+  'index.html',
+  'about.html',
+  'css/style.css',
+  'js/main.js'
+];
 
 // Call Install Event
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
 
-//   e.waitUntil(
-//     caches
-//       .open(cacheName)
-//       .then(cache => {
-//         console.log('Service Worker: Caching Files');
-//         cache.addAll(cacheAssets);
-//       })
-//       .then(() => self.skipWaiting())
-//   );
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
 // Call Activate Event
